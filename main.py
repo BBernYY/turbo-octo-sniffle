35ae9f78f3e59d3d3d1157852d0584965b592414
BBernYY <66414852+BBernYY@users.noreply.github.com>
2022-09-18 12:39:44 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-def main():
-    print("hello world!")
-
-
-
-if __name__ == '__main__': # checks if the code is ran as a file
-    main() # starts the main function
+import discord
+from datetime import datetime
+import discord.utils
+from discord.ext import commands
+import json
+import os
+bot = commands.Bot(intents=discord.Intents.default(), command_prefix='!')
+bot.remove_command('help')
+data = json.load(open("data.json"))
+def get_list(dictionary, key):
+  output = []
+  for k, v in dictionary.items():
+    output.append(v[key])
+  return output
+@bot.event
+async def on_ready():
+  print(f'startup at {datetime.now()}')
+@bot.command()
+async def help(ctx, command=None):
+    if command:
+      embed=discord.Embed(title=command, description=data["commands"][command]["description"])
+      embed.add_field(name="usage", value=data["commands"][command]["usage"])
+    else:
+      embed=discord.Embed(title='Help', description='commands and the description', color=0xFF5733)
+      embed.add_field(name="command", value="\n".join(list(data["commands"].keys())))
+      embed.add_field(name="description", value="\n".join(get_list(data["commands"], "description")))
+      embed.add_field(name="usage", value="\n".join(get_list(data["commands"], "usage")))
+    await ctx.channel.send("I'm glad to help!", embed=embed)
+# connect token
+bot.run(open('TOKEN.env').read())
